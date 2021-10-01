06f17f4787918236a68ad72090cf74a433b499f1
MohammadReza8080 <emohammadreza568@gmil.com>
2021-10-01 16:18:10 +0330

    Refactor all methods.

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Manage manager=new Manage();
+        manager.addCity(new City(1,"Mashhad"));
+        manager.addCity(new City(2,"Tehran"));
+        manager.addCity(new City(3,"Isfahan"));
+        manager.addCity(new City(4,"Tabriz"));
+        manager.addRoad(new Road(1,"as",1,2,null,120,1000,true));
+        manager.addRoad(new Road(2,"df",1,3,null,120,1000,true));
+        manager.addRoad(new Road(3,"gh",2,3,null,120,1000,true));
+        manager.addRoad(new Road(4,"gh",3,4,null,120,1000,true));
+
+        System.out.println(manager.getPaths(1,2));
     }
 }
--- src/ir/maktab/Manage.java
+++ src/ir/maktab/Manage.java
@@ -2,11 +2,12 @@ package ir.maktab;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Manage {
-    private TreeSet<City> cities = new TreeSet<City>();
-    private TreeSet<Road> roads = new TreeSet<Road>();
+    private ArrayList<City> cities = new ArrayList<City>();
+    private ArrayList<Road> roads = new ArrayList<Road>();
 
 
     public void addCity(City city) {
@@ -20,8 +21,8 @@ public class Manage {
     public boolean deleteCity(int id) {
         Iterator<City> it = cities.iterator();
         while (it.hasNext()) {
-            City temp= it.next();
-            if (temp.equals(findCityById(id))){
+            City temp = it.next();
+            if (temp.equals(findCityById(id))) {
                 it.remove();
                 return true;
             }
@@ -32,18 +33,19 @@ public class Manage {
     public City findCityById(int id) {
         Iterator<City> it = cities.iterator();
         while (it.hasNext()) {
-            City temp= it.next();
-            if (temp.equals(new City(id))){
+            City temp = it.next();
+            if (temp.equals(new City(id))) {
                 return temp;
             }
         }
         return null;
     }
+
     public boolean deleteRoad(int id) {
         Iterator<Road> it = roads.iterator();
         while (it.hasNext()) {
-            Road temp= it.next();
-            if (temp.equals(findCityById(id))){
+            Road temp = it.next();
+            if (temp.equals(findCityById(id))) {
                 it.remove();
                 return true;
             }
@@ -54,40 +56,47 @@ public class Manage {
     public Road findRoadById(int id) {
         Iterator<Road> it = roads.iterator();
         while (it.hasNext()) {
-            Road temp= it.next();
-            if (temp.equals(new Road(id))){
+            Road temp = it.next();
+            if (temp.equals(new Road(id))) {
                 return temp;
             }
         }
         return null;
     }
 
-    public ArrayList<String> getPaths(int originId, int destinationId){
-        ArrayList<Road> tempRoads=new ArrayList<Road>();
-        for (var road:roads) {
-            if(road.getOrigin()==originId)
+    public ArrayList<String> getPaths(int originId, int destinationId) {
+        ArrayList<Road> tempRoads = new ArrayList<Road>();
+        for (var road : roads) {
+            if (road.getOrigin() == originId)
                 tempRoads.add(road);
         }
-        ArrayList<String> result=new ArrayList<String>();
-        for (var roads:tempRoads) {
-            result.add(getDestination(roads,destinationId));
+        ArrayList<String> tempPath = new ArrayList<String>();
+        for (var roads : tempRoads) {
+            tempPath.add(getDestination(roads, destinationId));
+        }
+        ArrayList<String> result = new ArrayList<String>();
+        for (var path:tempPath) {
+            String[] roadId=path.split(" ");
+            if(findRoadById(Integer.parseInt(roadId[roadId.length-1])).getDestination()==destinationId)
+                result.add(path);
         }
         return result;
     }
 
     public String getDestination(Road road, int destinationId) {
-        if(road.getDestination()==destinationId)
-            return road.getId()+" ";
-        else{
-            ArrayList<Road> tempRoads=new ArrayList<Road>();
-            for (var tmpRoad:roads) {
-                if(tmpRoad.getOrigin()== road.getDestination())
-                    tempRoads.add(road);
+        if (road.getDestination() == destinationId)
+            return road.getId() + " ";
+        else {
+
+            ArrayList<Road> tempRoads = new ArrayList<Road>();
+            for (var tmpRoad : roads) {
+                if (tmpRoad.getOrigin() == road.getDestination())
+                    tempRoads.add(tmpRoad);
             }
-            for (var roads:tempRoads) {
-                return getDestination(roads,destinationId)+" ";
+            for (var r : tempRoads) {
+                return road.getId()+" " + getDestination(r, destinationId);
             }
         }
-           return "";
+        return "";
     }
 }
